92fb69ce26ec1fb2c25f0adbdd5164988e545e3f
chinjiaqing <chinjiaqing@qq.com>
2025-07-18 18:56:49 +0800

    debug

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ var GetDC = user32.func("HDC __stdcall GetDC(HWND hWnd)");
 var ReleaseDC = user32.func("int __stdcall ReleaseDC(HWND hWnd, HDC hDC)");
 var GetSystemMetrics = user32.func("int __stdcall GetSystemMetrics(int nIndex)");
 var CreateSolidBrush = gdi32.func("HBRUSH __stdcall CreateSolidBrush(uint32 color)");
-var FillRect = user32.func("int __stdcall FillRect(HDC hDC, LPRECT lprc, HBRUSH hbr)");
+var FillRect = user32.func("int __stdcall FillRect(HDC hdc, LPRECT lprc, HBRUSH hbr)");
 var DeleteObject = gdi32.func("int __stdcall DeleteObject(void* hObject)");
 function drawFullScreenRect() {
   const hdc = GetDC(null);
@@ -31,21 +31,19 @@ function drawFullScreenRect() {
   const width = GetSystemMetrics(0);
   const height = GetSystemMetrics(1);
   console.log(`\u5C4F\u5E55\u5C3A\u5BF8\uFF1A${width}x${height}`);
+  const fullScreenRect = {
+    left: 0,
+    top: 0,
+    right: width,
+    bottom: height
+  };
   const rectBuffer = koffi.alloc(RECT, 1);
-  const nodeBuffer = rectBuffer.buffer;
-  if (!nodeBuffer) {
-    console.error("\u65E0\u6CD5\u83B7\u53D6\u5E95\u5C42Node.js Buffer\uFF08koffi\u7248\u672C\u4E0D\u517C\u5BB9\uFF09");
-    ReleaseDC(null, hdc);
-    return;
-  }
-  nodeBuffer.writeInt32LE(0, 0);
-  nodeBuffer.writeInt32LE(0, 4);
-  nodeBuffer.writeInt32LE(width, 8);
-  nodeBuffer.writeInt32LE(height, 12);
+  koffi.write(rectBuffer, RECT, fullScreenRect);
   const brush = CreateSolidBrush(65280);
   if (!brush) {
     console.error("CreateSolidBrush \u5931\u8D25");
     ReleaseDC(null, hdc);
+    koffi.free(rectBuffer);
     return;
   }
   const ret = FillRect(hdc, rectBuffer, brush);
